f754e54e1f65bae3b41fbc06bc237b6b1ad22c2c
Hackbright student <info@hackbrightacademy.com>
2018-10-08 12:51:51 -0700

    Handles variation in capitalization

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,16 @@
 def wordcount(file_name):
 	"""takes in a file and prints out the set of words and the number of times each word occurs in the text file"""
 	
-	word_counts = {}
-	all_words = []
+	word_counts = {}										#create empty dictionary
+	all_words = []											#create empty list that will store words
 	
 	with open(file_name) as file:
-		for line in file:
-			line = line.rstrip() #removes /n from end of lines
-			words = line.split(" ")
-			all_words.extend(words) 
+		for line in file:			
+			line = line.rstrip() 							#removes /n from end of lines
+			words = line.split(" ")							#split phrase where there are spaces
+			all_words.extend(words) 						#put words into a single list
 	for word in all_words:
+		word = word.lower()									#all words in lowercase
 		word_counts[word] = word_counts.get(word, 0) + 1		
 	
 	for word in word_counts:
